c31c4262e5b5a96dedc58db4aa9d3fac815c7d2f
wattashira <anajamx@gmail.com>
2023-12-14 21:57:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# obstacleAssault
+ 
---
+++ Source/obstacleAssault/movingPlatform.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "movingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AmovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AmovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere)
+	int32 MyInt=99;
+	UPROPERTY(EditAnywhere)
+	int32 MyInt2=200;
+	UPROPERTY(EditAnywhere)
+	float MyFloat=5.5;
+
+};
